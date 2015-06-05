2ec9f9387e3ee2e8908ca111059695b1b5d9d8f9
aloisdg <aloisdegouvello@live.fr>
2015-06-05 11:14:38 +0200

    Add exercise 1

---
+++ Module3/app.ts
@@ -0,0 +1,15 @@
+﻿window.onload = () => {
+
+    var firstname: string;
+    var lastname: string;
+    var fullname: string;
+
+    firstname = 'joe';
+    lastname = 'smith';
+    fullname = firstname + " " + lastname;
+
+    var e: Null;
+
+    var el = document.getElementById('content');
+    el.innerHTML += fullname;
+};
