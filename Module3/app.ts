70827aeb32d0e4ba0e58b2f5611cebc2f1851e9a
aloisdg <aloisdegouvello@live.fr>
2015-06-05 11:25:31 +0200

    Add exercise 3

--- Module3/app.ts
+++ Module3/app.ts
@@ -1,12 +1,12 @@
 ﻿window.onload = () => {
-    var el = document.getElementById('content');
+    var el = document.getElementById("content");
 
     var firstname: string;
     var lastname: string;
     var fullname: string;
 
-    firstname = 'joe';
-    lastname = 'smith';
+    firstname = "joe";
+    lastname = "smith";
     fullname = firstname + " " + lastname;
 
     el.innerHTML += fullname;
@@ -15,14 +15,19 @@
     var y: number;
     var a: string;
 
-    a = 'nine';
+    a = "nine";
 
-    x = 'five';
+    x = "five";
     y = 7;
     a = x + y;
 
     el.innerHTML += " " + a;
 
-
-    
+    var pets: string[];
+    var myPets: string;
+    pets = ["fido", "spot", "seeker"];
+    pets.forEach(concat => {
+	    myPets += concat + ", ";
+    });
+    el.innerHTML += "My pets are " + " " + pets;
 };
